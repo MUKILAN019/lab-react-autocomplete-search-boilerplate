ca989eed7a63166b619aee21bf90417e55e0cd6f
MUKILAN019 <mukilan.p@kalvium.community>
2024-02-24 17:48:00 +0530

    added files

---
+++ Search/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import data from "./countryData.json";
+
+function App() {
+  const [state, setState] = useState([]);
+  
+  const handleChange = (event) => {
+    const eventName = event.target.value.toLowerCase();
+    const filteredData = data.filter(item => item.name.toLowerCase().includes(eventName));
+    setState(filteredData);
+  }
+
+  const handleKey = (event) => {
+    if(event.key === "Escape"){
+      console.log("Escape")
+      setState([])
+    }
+  }
+
+  return (
+    <div>
+      <h1>Search</h1>
+      <input type="text" onChange={handleChange} onKeyDown={handleKey}/>
+      <button>Search</button>
+      {state.map((item, index) => (
+        <div key={index}>{item.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
